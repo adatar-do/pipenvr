bc020a74d34134b6c82a81cdef7f4b6a0f94cf1e
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-12-16 21:38:06 -0400

    feat (use_pipenv): use_pipenv function working

--- R/use_pipenv.R
+++ R/use_pipenv.R
@@ -1,15 +1,86 @@
-use_pipenv <- function(required = FALSE){
-  is_venv_active = reticulate::py_config()[["virtualenv"]]
-  if(is_venv_active == ""){
-    are_venv = system("python -m pipenv --python 3.6 --venv")
-    if(are_venv == 1){
-      system("python -m pipenv shell --python 3.6")
-    }
-    venv = system("python -m pipenv --python 3.6 --venv", intern = T)
-    #Sys.setenv(RETICULATE_PYTHON = venv)
+#' Use pipenv with reticulate
+#'  If a pipenv environment exists, use it. Otherwise, create a new one.
+#'  Set the RETICULATE_PITHON environment variable to the path of the
+#'  pipenv virtual environment. This process is only done once per project.
+#'
+#' @param args [character] Other arguments to pass to \code{pipenv shell {{args}} }.
+#'
+#' @return NULL
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' pipenvr::use_pipenv()
+#' }
+use_pipenv <- function(args = "") {
+  is_venv_active <- reticulate::py_config()[["virtualenv"]]
+  if (is_venv_active == "") {
+    create_pipenv(args)
+    python <- get_python_path()
+    set_env_var(python)
+    print(paste0("Using pipenv virtual environment at ", python))
   } else {
-    print("virtual environment already activated. \n
-          No action taken to avoid nested environments.")
+    print("virtual environment already activated.
+    No action taken to avoid nested environments.")
   }
-  return(venv)
+}
+
+
+create_pipenv <- function(args) {
+  are_venv <- system("python -m pipenv --venv", intern = T)
+  if (are_venv == 1) {
+    if(args == ""){
+    system("python -m pipenv shell")
+    } else {
+      system(paste0("python -m pipenv shell ", args))
+    }
+  }
+}
+
+
+get_python_path <- function() {
+    venv <- system("python -m pipenv --venv", intern = T)
+    suffix <- if (identical(.Platform$OS.type, "windows")) {
+      "Scripts/python.exe"
+    } else {
+      "bin/python"
+    }
+    python <- paste0(
+      stringr::str_remove_all(
+        file.path(venv),
+        "\\r"
+      ),
+      "\\",
+      suffix
+    )
+    python <- stringr::str_replace_all(python, "\\\\", "/")
+}
+
+
+set_env_var <- function(python) {
+    project <- getwd()
+    #name <- basename(project)
+    env_file <- paste0(project, "/", ".Renviron")
+    if (!file.exists(env_file)) {
+      env_cont <- paste0("RETICULATE_PYTHON=", python)
+    } else {
+      env_cont <- readr::read_file(env_file)
+      pattern <- "RETICULATE_PYTHON=.*[Scripts/python.exe]?[bin/python]?"
+      have_env <- stringr::str_detect(env_cont, pattern)
+      add_env <- paste0("RETICULATE_PYTHON=", python)
+
+      if (have_env) {
+        env_cont <- stringr::str_replace(
+          env_cont,
+          stringr::regex(pattern),
+          add_env
+        )
+      } else {
+        env_cont <- paste0(env_cont, add_env)
+      }
+    }
+    env_cont <- stringr::str_remove_all(env_cont, stringr::regex("^[\n\r]"))
+    env_cont <- stringr::str_remove_all(env_cont, stringr::regex("[\n\r]$"))
+    write(env_cont, env_file)
 }
